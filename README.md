00844fa88f1e19b2e7c4ee58aff5e2013cd14c84
dmassiani <davidmassianirennes@gmail.com>
2014-12-01 08:47:47 +0100

    renamed

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# [Poots Starter Theme]
+# [Loots Starter Theme]
 
-Poots is a WordPress starter theme forked on root theme based on [HTML5 Boilerplate](http://html5boilerplate.com/) & [Bourbon, Neat, Bitters](http://http://bourbon.io/) that will help you make better themes with Gulp.
+Loots is a WordPress starter theme forked on root theme based on [HTML5 Boilerplate](http://html5boilerplate.com/) & [Bourbon, Neat, Bitters](http://http://bourbon.io/) that will help you make better themes with Gulp.
--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,10 @@
 */
 
 // Project configuration
-var project     = 'poots', // Optional - Use your own project name here...
+var project     = 'loots', // Optional - Use your own project name here...
 	build       = './build/', // Files that you want to package into a zip go here
 	source      = './assets/', 	// Your main project assets and naming 'source' instead of 'src' to avoid confusion with gulp.src
-	bower       = './bower_components/'; // Not truly using this yet, more or less playing right now. TO-DO Place in Dev branch
+	bower       = './bower/'; // Not truly using this yet, more or less playing right now. TO-DO Place in Dev branch
 
 // Load plugins
 var gulp = require('gulp'),
